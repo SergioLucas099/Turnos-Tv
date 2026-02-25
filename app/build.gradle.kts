c2bffbbaed0c51abc743d8f4e6a48cc840866887
Sergio Lucas <155183770+SergioLucas099@users.noreply.github.com>
2026-02-25 14:39:10 -0500

    ajustes ApiClient

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,5 +56,6 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("io.ktor:ktor-client-okhttp:2.3.7")
 
 }
--- app/src/main/java/com/example/turnostv/network/ApiClient.kt
+++ app/src/main/java/com/example/turnostv/network/ApiClient.kt
@@ -4,14 +4,26 @@ import io.ktor.client.*
 import io.ktor.client.engine.android.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.engine.okhttp.OkHttp
 
 object ApiClient {
 
-    val client = HttpClient(Android) {
+    val client = HttpClient(OkHttp) {
         install(ContentNegotiation) {
             json()
         }
     }
 
-    const val BASE_URL = "http://192.168.2.118:8080"
+    const val BASE_URL = "http://192.168.0.200:8080"
+
+    //⚠️ IMPORTANTE
+    //Reemplaza IP por la IP de tu PC en red local.
+
+//    Adaptador de Ethernet Ethernet 4:
+//
+//    Sufijo DNS específico para la conexión. . : www.tendawifi.com
+//    Vínculo: dirección IPv6 local. . . : fe80::a301:b3fd:9469:8ce2%22
+//    Dirección IPv4. . . . . . . . . . . . . . : 192.168.0.200
+//    Máscara de subred . . . . . . . . . . . . : 255.255.255.0
+//    Puerta de enlace predeterminada . . . . . : 192.168.0.1
 }
